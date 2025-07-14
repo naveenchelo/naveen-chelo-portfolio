351b6fb61a9166646cf95b35124b83c05c799549
NaveenChelo <chelonaveen07@gmail.com>
2025-07-14 20:57:44 +0530

    created portfolio structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './features/home/home.component';
+import { AboutComponent } from './features/about/about.component';
+import { ExperienceComponent } from './features/experience/experience.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { SkillsComponent } from './features/skills/skills.component';
+import { ContactComponent } from './features/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
